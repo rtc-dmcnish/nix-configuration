40fe1f2029697e0ad999840a47843a272f99d689
Doug McNish <123575804+rtc-dmcnish@users.noreply.github.com>
2026-09-23 10:41:14 -0400

    fix: update taskwarrior report filters to use string format

--- home.nix
+++ home.nix
@@ -20,27 +20,28 @@
 
   programs.taskwarrior = {
     enable = true;
+    package = pkgs.taskwarrior3;
     config = {
       data.location = "~/.task";
       news.version = "3.5.0";
       sync.local.server_dir="~/Library/Mobile Documents/iCloud~com~mav~taskchamp/Documents/taskchamp";
       dateformat = "Y-M-D";
       report.next = {
-        filter = [ +PENDING -BLOCKED -WAITING -hold limit:page -p ];
+        filter =  "+PENDING -BLOCKED -WAITING -hold limit:page -p";
       }; 
       report.work = {
         description = "Work Tasks"; 
         columns = [ "id" "start" "entry.age" "depends" "priority" "project" "tags" "recur" "scheduled" "due" "until" "description" ];
         labels = [ "ID" "Active" "Age" "Deps" "P" "Project" "Tags" "Recur" "Sched" "Due" "Until" "Description" ];
         sort = [ "priority-" "due+" "project+" "entry+" ];
-        filter = [ +PENDING -WAITING -hold limit:page -p ];
+        filter = "+PENDING -WAITING -hold limit:page -p";
       };
       report.home = {
         description = "Home Tasks"; 
         columns = [ "id" "start" "entry.age" "depends" "priority" "project" "tags" "recur" "scheduled" "due" "until" "description" ];
         labels = [ "ID" "Active" "Age" "Deps" "P" "Project" "Tags" "Recur" "Sched" "Due" "Until" "Description" ];
         sort = [ "priority-" "due+" "project+" "entry+" ];
-        filter = [ +PENDING -WAITING -hold limit:page +p ];
+        filter = "+PENDING -WAITING -hold limit:page +p";
       };
     };
   };
